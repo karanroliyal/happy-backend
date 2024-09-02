52e02da126f60fb4e2588ca80474378deaee91a4
karanroliyal <91712507+karanroliyal@users.noreply.github.com>
2024-09-02 16:04:56 +0530

    first commit

---
+++ src/api/home-page-banner/controllers/home-page-banner.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * home-page-banner controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::home-page-banner.home-page-banner');
---
+++ src/api/home-page-banner/routes/home-page-banner.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * home-page-banner router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::home-page-banner.home-page-banner');
